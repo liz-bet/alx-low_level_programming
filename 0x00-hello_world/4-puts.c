6a45fc5ea73643bde9d594bdcd582ea9f9db022d
liz-bet <elizabethukpabio@yahoo.com>
2022-04-19 04:09:20 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+
+	return (0);
+
+}
